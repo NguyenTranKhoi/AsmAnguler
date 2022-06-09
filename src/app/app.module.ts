692912081884b406855377cf2d3590e3075c882f
khoi <khoidolce@gmail.com>
2022-06-09 13:20:17 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './layouts/home/home.component';
+import { BlogComponent } from './layouts/blog/blog.component';
+import { WorksComponent } from './layouts/works/works.component';
+import { DetailWorksComponent } from './layouts/detail-works/detail-works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    WorksComponent,
+    DetailWorksComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layouts/detail-works/detail-works.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-works',
+  templateUrl: './detail-works.component.html',
+  styleUrls: ['./detail-works.component.css']
+})
+export class DetailWorksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
