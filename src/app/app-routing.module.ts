cdf48f63e94d1c7eef666be28662e49d3f42f3b6
khoi <khoidolce@gmail.com>
2022-06-09 13:32:30 +0700

    feact: layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './layouts/blog/blog.component';
+import { DetailWorksComponent } from './layouts/detail-works/detail-works.component';
+import { HomeComponent } from './layouts/home/home.component';
+import { WorksComponent } from './layouts/works/works.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "", component: BlogComponent },
+  { path: "", component: WorksComponent },
+  { path: "", component: DetailWorksComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
